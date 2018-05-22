ff9d937b2f0681d6369ae68d8a5692ce08c1fcc8
Nick Glyzhko <supreeemeeee@gmail.com>
2018-05-22 23:20:30 +0300

    update and adding sass

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="calculator">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum neque, explicabo vel quis ipsam fugiat laboriosam eligendi qui nihil cumque cupiditate, amet quasi quisquam dolor quae maiores minus? Inventore, placeat.
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+$background: #000;
+#calculator {
+  background: $background;
+}
+</style>
+
+
