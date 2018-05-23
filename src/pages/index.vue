43097c1421aa834d3055cd494a836339ec3018af
Nick Glyzhko <supreeemeeee@gmail.com>
2018-05-23 17:52:54 +0300

    HTML and CSS template (with update of index.js)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,30 @@
 <template>
   <div id="calculator">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum neque, explicabo vel quis ipsam fugiat laboriosam eligendi qui nihil cumque cupiditate, amet quasi quisquam dolor quae maiores minus? Inventore, placeat.
+    <div class="window"></div>
+    <div class="buttons">
+      <button>CE</button>
+      <button>C</button>
+      <button>
+        <=</button>
+          <button>%</button>
+          <button class="number">7</button>
+          <button class="number">8</button>
+          <button class="number">9</button>
+          <button>/</button>
+          <button class="number">4</button>
+          <button class="number">5</button>
+          <button class="number">6</button>
+          <button>*</button>
+          <button class="number">1</button>
+          <button class="number">2</button>
+          <button class="number">3</button>
+          <button>-</button>
+          <button disabled>.</button>
+          <button class="number">0</button>
+          <button>=</button>
+          <button>+</button>
+
+    </div>
   </div>
 </template>
 
@@ -9,9 +33,46 @@ export default {}
 </script>
 
 <style lang="scss">
-$background: #000;
+$bg-grey: #e8e8e8;
+$bg-symbols: #f2f2f2;
+$bg-numbers: #fafafa;
+$bg-disabled: #e3e3e3;
 #calculator {
-  background: $background;
+  background: $bg-grey;
+  margin: 60px auto;
+  width: 400px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  .window {
+    background: #fff;
+    height: 50px;
+    width: 100%;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+  }
+  .buttons {
+    display: grid;
+    grid: 40px / repeat(4, 1fr);
+    grid-auto-rows: 40px;
+    gap: 4px;
+    button {
+      background: $bg-symbols;
+      border: 1px solid #ccc;
+      &:focus {
+        outline: none;
+      }
+      &.number {
+        background: $bg-numbers;
+      }
+      &:hover {
+        background: #fff;
+      }
+      &:disabled {
+        background: $bg-disabled;
+        cursor: not-allowed;
+      }
+    }
+  }
 }
 </style>
 
